0990eabf682b6db8f6cf2e193cef24e40ed54c7b
Abhinav Prasad <abhinavprasad0209@gmail.com>
2024-07-02 16:35:02 +0530

    first commit: user model, validate resource middleware

---
+++ config/default.ts
@@ -0,0 +1,6 @@
+export default {
+  port: 1337,
+  dburi:
+    "mongodb+srv://abhinavprasad0209:restnigtest@resttest.mr236ol.mongodb.net/?retryWrites=true&w=majority&appName=restTest",
+  saltWorkFactor: 10, // salt rounds
+};
